f58692ee180f168affdca0b502573028cec3d1be
omognuni <melbong0069@gmail.com>
2022-12-21 11:55:37 +0900

    [Feat] 유저 및 가계부 내역 모델 생성

---
+++ account_book/core/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        '''유저 생성'''
+        if not email:
+            raise ValueError('메일을 입력하세요')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        '''superuser 생성'''
+        user = self.create_user(email, password)
+
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    '''email을 username으로 사용하는 유저 모델'''
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+
+class Record(models.Model):
+    '''가계부 내역'''
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    category = models.CharField(max_length=255, blank=True)
+    amount = models.BigIntegerField(blank=True, default=0)
+    memo = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
