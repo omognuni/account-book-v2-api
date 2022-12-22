77ef34f5a082daa16c74acb05049bcf0f7704253
omognuni <melbong0069@gmail.com>
2022-12-23 08:30:42 +0900

    [Feat] 가계부 내역 구현

--- account_book/account_book/urls.py
+++ account_book/account_book/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from record.views import redirect_url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/user/', include('user.urls')),
+    path('api/v1/record/', include('record.urls')),
+    path('<new_url>', redirect_url)
 ]
