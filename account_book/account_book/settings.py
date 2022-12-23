905fc2fe5ca03c16d02f0138630e89021ab2adc7
omognuni <melbong0069@gmail.com>
2022-12-23 16:31:22 +0900

    [Chore] jwt auth

--- account_book/account_book/settings.py
+++ account_book/account_book/settings.py
@@ -149,8 +149,6 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
