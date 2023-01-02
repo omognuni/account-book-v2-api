2ddc2e77e7da105548a9336d75120852ad2f89a1
omognuni <melbong0069@gmail.com>
2023-01-02 18:27:54 +0900

    [Chore] rest authtoken 삭제

--- account_book/account_book/settings.py
+++ account_book/account_book/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django_redis',
     'drf_spectacular',
     'rest_framework',
-    'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'core',
